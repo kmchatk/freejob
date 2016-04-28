176040934db5a085ebaec9cc0d41a8213b34e0e8
Bogdan Constantin <bogdan.alexandru@gmail.com>
2016-04-28 13:50:14 +0300

    Added new files

---
+++ rest/src/main/java/com/itsix/freejob/rest/service/Locations.java
@@ -0,0 +1,43 @@
+package com.itsix.freejob.rest.service;
+
+import java.util.UUID;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.itsix.freejob.core.Location;
+import com.itsix.freejob.core.exceptions.FreeJobException;
+import com.itsix.freejob.rest.OsgiRestResource;
+import com.itsix.freejob.rest.data.Result;
+
+public class Locations extends OsgiRestResource {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Result createLocation(@PathParam("userId") UUID userId,
+            Location location) throws FreeJobException {
+        UUID locationId = getApi().createLocation(userId, location);
+        return Result.ok(locationId);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Result listLocations(@PathParam("userId") UUID userId) {
+        return Result.ok(getApi().listLocations(userId));
+    }
+
+    @DELETE
+    @Path("{locationId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Result deleteLocation(@PathParam("locationId") UUID locationId) {
+        getApi().deleteLocation(locationId);
+        return Result.ok();
+    }
+}
